5576c3b237af7d224198c50f901944ed99fab908
DilipDixit <dilipkumar.dixit@tantra.io>
2023-12-20 12:31:38 +0530

    UserRoles added

---
+++ Models/UserRole.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+
+namespace UserManagementDummy.Models
+{
+    public class UserRole
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+       // public User User { get; set; } = null!;
+
+        public int RoleID { get; set; }
+        //public Role Role { get; set; } = null!;
+    }
+}

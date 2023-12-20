10a7d5ba79b76e516706a78b52d969ec069fe5d3
Chaitali241 <chaitali.trivedi@tntra.io>
2023-12-20 12:24:07 +0530

    Added Role model

---
+++ Models/Role.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagementDummy.Models
+{
+    
+    public class Role
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Permissions { get; set; }
+
+       // public List<UserRole> UserRoles { get; set; } = new List<UserRole>();
+    }
+}

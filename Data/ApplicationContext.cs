d2f248e67999728324dd7b5b67a1eae5c82f7407
Chaitali241 <chaitali.trivedi@tntra.io>
2023-12-20 18:39:29 +0530

    chnage names

--- Data/ApplicationContext.cs
+++ Data/ApplicationContext.cs
@@ -10,9 +10,9 @@ namespace UserManagementDummy.Data
             
         }
 
-        public DbSet<User> User { get; set; }
-        public DbSet<UserRole> UserRole { get; set; }
-        public DbSet<Role> Role { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<Role> Roles { get; set; }
 
     }
 }

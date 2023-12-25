1a8733a33849a2636e5d59ff34aac2ca2528859c
Chaitali241 <chaitali.trivedi@tntra.io>
2023-12-25 11:25:30 +0530

    Added migration

---
+++ Migrations/20231222125211_RecreateUserRoleTableNEwNAMe.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserManagementDummy.Migrations
+{
+    public partial class RecreateUserRoleTableNEwNAMe : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

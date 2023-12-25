7b78ffd7a2a22d7ab72e985a8ea1950e9f1eabe8
DilipDixit <dilipkumar.dixit@tantra.io>
2023-12-25 16:41:53 +0530

    some controller and models and program has changed

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,14 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using System;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
 using UserManagementDummy.Data;
 using UserManagementDummy.Models;
 
@@ -17,6 +13,7 @@ namespace UserManagementDummy.Controllers
     [Route("api/[controller]")]
     [ApiController]
     [AllowAnonymous]
+
     public class LoginController : ControllerBase
     {
         private readonly ApplicationContext context;
@@ -26,7 +23,7 @@ namespace UserManagementDummy.Controllers
             this.context = context;
         }
 
-        [HttpGet]
+        [HttpPost]
         public async Task<ActionResult<User>> Login(int? id, string password)
         {
             if (!id.HasValue || string.IsNullOrEmpty(password))
@@ -34,7 +31,6 @@ namespace UserManagementDummy.Controllers
                 return BadRequest("Invalid input. Both ID and password are required.");
             }
 
-            // Fetch the user from the database based on the provided ID
             var user = await context.Users.Where(u => u.ID == id).FirstOrDefaultAsync();
 
             if (user == null)
@@ -47,16 +43,18 @@ namespace UserManagementDummy.Controllers
                 return Unauthorized("Invalid password.");
             }
 
-            // If authentication is successful, generate a JWT token
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = new byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(key);
+            }
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(new Claim[]
                 {
-                    new Claim(ClaimTypes.Name, user.ID.ToString()),
-                    new Claim(ClaimTypes.Email,user.Email.ToString())
-                    
+                    new Claim(ClaimTypes.Name, user.FirstName.ToString()),
+                    new Claim(ClaimTypes.Email, user.Email.ToString())
                 }),
                 Expires = DateTime.UtcNow.AddHours(1),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
@@ -64,14 +62,12 @@ namespace UserManagementDummy.Controllers
             var token = tokenHandler.CreateToken(tokenDescriptor);
             var tokenString = tokenHandler.WriteToken(token);
 
-            // Return the authenticated user along with the JWT token
             return Ok(new { user, Token = tokenString });
         }
 
         private bool ValidatePassword(User user, string password)
         {
-            // for password validation using a secure password hashing algorithm either bcrypt or Argon2
-            return user.Password == password; 
+            return user.Password == password;
         }
     }
 }
